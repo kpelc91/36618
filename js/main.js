34c61f0ce36801e01af09ece8f1d176ceeeeac2e
kpelc <kamilapelc01@gmail.com>
2022-04-03 15:28:47 +0200

    konsola wygląda całkiem ok, ale kod brzydki :)

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let name = 'Arbuz';
+console.log(name);
+let number = 1;
+console.log('1');
+
+
+name = 'Melon'
+console.log(name);
+number = '2'
+console.log(number);
+
+
+name = 'Banan';
+console.log(name);
+number = '3'
+console.log(number);
+
+
+name = 'Napoje';
+console.log(name);
+let array = ['Arbuz', 'Melon', 'Banan'];
+console.log(array);
